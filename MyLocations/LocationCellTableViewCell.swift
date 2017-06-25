2d04ff5d78d7991b0b10584deb5355b6e93a147c
mantonelli01a <mike@mantonelli.plus.com>
2017-06-25 16:28:56 +0100

    again

---
+++ MyLocations/LocationCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCellTableViewCell.swift
+//  MyLocations
+//
+//  Created by Administrator on 25/06/2017.
+//  Copyright © 2017 Michael Antonelli. All rights reserved.
+//
+
+import UIKit
+
+class LocationCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
